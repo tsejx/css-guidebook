db3a56998cc83c70f28bd7f0ec03f1148bb42bb6
tsejx <tsejx@foxmail.com>
2020-09-16 22:36:00 +0800

    :fire: remove: remove .umi files

--- .umi/core/plugin.ts
+++
@@ -1,7 +0,0 @@
-import { Plugin } from '/Users/mrsingsing/mrsingsing/CSS-GuideBook/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['patchRoutes','rootContainer','render','onRouteChange',],
-});
-
-export { plugin };
