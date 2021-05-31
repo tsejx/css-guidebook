ae1f002819a7f0d943d03718eadf8a66a053c8c9
tsejx <tsejx@foxmail.com>
2021-05-31 22:47:13 +0800

    chore: update dumi config

--- config/config.js
+++ config/config.js
@@ -6,8 +6,8 @@ const config = {
   publicPath: '/css-guidebook/',
   favicon: './favicon.ico',
   logo: 'http://img.mrsingsing.com/css-guidebook-favicon.png',
-  // exportStatic: {},
-  // dynamicImport: {},
+  hash: true,
+  exportStatic: {},
   navs: [
     null,
     {
@@ -27,8 +27,8 @@ const config = {
   ],
 };
 
-// if (process.env.NODE_ENV !== 'development') {
-//   config.ssr = {};
-// }
+if (process.env.NODE_ENV !== 'development') {
+  config.ssr = {};
+}
 
 export default config;
