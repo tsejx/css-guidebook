5d0cba2d04ec806c9405b102d192eb51c0b315fb
tsejx <tsejx@foxmail.com>
2021-05-31 22:45:47 +0800

    docs: update

--- config/config.js
+++ config/config.js
@@ -6,8 +6,8 @@ const config = {
   publicPath: '/css-guidebook/',
   favicon: './favicon.ico',
   logo: 'http://img.mrsingsing.com/css-guidebook-favicon.png',
-  exportStatic: {},
-  dynamicImport: {},
+  // exportStatic: {},
+  // dynamicImport: {},
   navs: [
     null,
     {
@@ -17,7 +17,7 @@ const config = {
   ],
   extraBabelPlugins: [
     [
-      'babel-plugin-import',
+      'import',
       {
         libraryName: 'antd',
         libraryDirectory: 'es',
@@ -27,8 +27,8 @@ const config = {
   ],
 };
 
-if (process.env.NODE_ENV !== 'development') {
-  config.ssr = {};
-}
+// if (process.env.NODE_ENV !== 'development') {
+//   config.ssr = {};
+// }
 
 export default config;
